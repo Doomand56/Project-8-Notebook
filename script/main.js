06b4d4b9151267f8cb8579bdd7d2fe59330b97cf
Dooomand56 <doomand56@gmail.com>
2022-05-09 14:00:14 +0300

    created body

---
+++ script/main.js
@@ -0,0 +1,20 @@
+window.onload = function () {
+	
+
+	let inputAdd = document.getElementById('input-add');
+	let buttonAdd = document.getElementById('btn-add');
+
+	let outContent = document.getElementById('out-content');
+	let newContent = document.createElement('div');
+
+	function AddContent (item) {
+
+	}
+
+	buttonAdd.onclick = function () {
+
+	}
+
+
+	console.log();
+}
