123cd5ba6c0296667a727cf466bdb98dfd82c6be
Dooomand56 <doomand56@gmail.com>
2022-05-15 22:29:53 +0300

    Version 0.2

--- redme.md
+++ redme.md
@@ -1,3 +1,3 @@
 # Nodebook
 
-Version 0.1
+Version 0.2
--- script/main.js
+++ script/main.js
@@ -5,16 +5,52 @@ window.onload = function () {
 	let buttonAdd = document.getElementById('btn-add');
 
 	let outContent = document.getElementById('out-content');
-	let newContent = document.createElement('div');
 
-	function AddContent (item) {
-
-	}
 
 	buttonAdd.onclick = function () {
 
+		
+		let newContent = document.createElement('div');
+		let paragraph = document.createElement('p');
+		let img = document.createElement('img');
+
+		let editButton = document.createElement('button');
+		let removeButton = document.createElement('button');
+
+
+		img.setAttribute('src', 'img/1.gif');
+
+		editButton.innerHTML = 'Edit';
+		removeButton.innerHTML = 'Remove';
+
+		newContent.classList.add('block');
+		paragraph.innerHTML = inputAdd.value;
+
+
+
+
+
+		removeButton.onclick = () => {
+			newContent.remove();
+		}
+		editButton.onclick = () => {
+			result = prompt('Edit');
+			paragraph.innerHTML = result;
+			img.remove();
+		}
+
+
+
+
+		if (paragraph.innerHTML == '') {
+			newContent.appendChild(img);
+		}
+
+		outContent.appendChild(newContent);
+		newContent.appendChild(paragraph);
+		newContent.appendChild(editButton);
+		newContent.appendChild(removeButton);
 	}
 
 
-	console.log();
 }
